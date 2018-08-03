45bc854ccb0e30b1c4b7f322ff56aaabbd010ccd
Luis Molina <lualmoba128@gmail.com>
2018-08-02 22:17:08 -0500

    Adding virtual environment

---
+++ myTunes/myTunes_env/Scripts/html2text-script.py
@@ -0,0 +1,10 @@
+#!d:\dropbox\personal files\assignments\cs 564 - database management\project\musicapp\mytunes\mytunes_env\scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'html2text==2014.12.29','console_scripts','html2text'
+__requires__ = 'html2text==2014.12.29'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('html2text==2014.12.29', 'console_scripts', 'html2text')()
+    )
--- myTunes/tunesApp/views.py
+++ myTunes/tunesApp/views.py
@@ -5,3 +5,19 @@ from django.shortcuts import render
 def home(request):
 	"""The home page for the myTunes app."""
 	return render (request, 'base.html')
+	
+def favorites(request):
+	"""Favorites landing page."""
+	return render(request, 'base.html')
+	
+def favSongs(request):
+	"""Favorite songs for the user."""
+	return render(request, 'base.html')
+	
+def favArtists(request):
+	"""Favorite artists for the user."""
+	return render(request, 'base.html')
+	
+def favGenres(request):
+	"""Favorite genres for the user."""
+	return render(request, 'base.html')
