367b9f67b7787410a1f6fe41ff316aa5f25cf6ab
Luis Molina <lualmoba128@gmail.com>
2018-08-04 18:31:30 -0500

    Allow loading static files

--- myTunes/myTunes/settings.py
+++ myTunes/myTunes/settings.py
@@ -129,6 +129,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 AUTHENTICATION_BACKENDS = (
     #'userena.backends.UserenaAuthenticationBackend',
     #'guardian.backends.ObjectPermissionBackend',
