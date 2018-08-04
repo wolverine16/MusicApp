d174bdb53bfee6af8b8fde18a43ac14f83727669
Luis Molina <lualmoba128@gmail.com>
2018-08-04 11:31:18 -0500

    Updated base template, home page, and login page

--- myTunes/tunesApp/views.py
+++ myTunes/tunesApp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 def home(request):
 	"""The home page for the myTunes app."""
-	return render (request, 'base.html')
+	return render (request, 'home.html')
 	
 def favorites(request):
 	"""Favorites landing page."""
