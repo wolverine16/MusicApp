2ed6ed00bcc38bf0b1f7fc61248feb4df50ad5cb
Shiva <shivakumar.ramakrishnan1216@gmail.com>
2018-08-05 22:14:04 -0500

    update to fix errors

--- myTunes/tunesApp/forms.py
+++ myTunes/tunesApp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.forms import formset_factory
 
-class SearchForm(forms.Form):
+class SearchForm(forms.ModelForm):
 	song_name = forms.CharField(label = 'song', max_length = 150, required = False)
 	artist_name = forms.CharField(label = 'artist', max_length = 150, required = False)
 	genre_name = forms.CharField(label = 'genre', max_length = 150, required = False)
@@ -10,14 +10,14 @@ class SearchForm(forms.Form):
 	end_yr = forms.IntegerField(label = 'end_year', required = False)
 
 
-class SearchResults(forms.Form):
-	song_id = forms.CharField(max_length = 30, required = False, widget = HiddenInput())
+class SearchResults(forms.ModelForm):
+	song_id = forms.CharField(max_length = 30, required = False, widget = forms.HiddenInput())
 	title = forms.CharField(label = 'song', max_length = 150, required = False)
-	genre_id = forms.IntegerField(required = False, widget = HiddenInput())
+	genre_id = forms.IntegerField(required = False, widget = forms.HiddenInput())
 	label = forms.CharField(label = 'genre', max_length = 150, required = False)
-	artist_id = forms.CharField(max_length = 30, required = False, widget = HiddenInput())
+	artist_id = forms.CharField(max_length = 30, required = False, widget = forms.HiddenInput())
 	artist_name = forms.CharField(label = 'artist', max_length = 150)
-	album_id = forms.IntegerField(required = False, widget = HiddenInput())
+	album_id = forms.IntegerField(required = False, widget = forms.HiddenInput())
 	album_name = forms.CharField(label = 'album', max_length = 150, required = False)
 	year = forms.IntegerField(label = 'year')
 	add_to_fav = forms.BooleanField(label = 'is_fav')
